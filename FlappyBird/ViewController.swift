c6247991384d53dce2a010a4be22691066f1b971
yshiina1102 <yshiina@salesforce.com>
2020-05-05 15:21:58 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Yusuke Shiina on 2020/05/05.
+//  Copyright © 2020 Yusuke Shiina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
